c39b13c60914a98806024aacb467d7eb7dd9dd98
SANGRAMSINH MORE <sn996044@dal.ca>
2022-07-07 20:22:41 -0300

    frontend

---
+++ FrontEnd/newsbot/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavbarComp from './Components/Navbar';
+import { Grid } from "@mui/material";
+
+function App() {
+  return (
+    <Grid >
+      <NavbarComp />
+    </Grid>
+  );
+}
+
+export default App;
