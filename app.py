5975e5008e44a88d8e278a702e68c17df2fd3fd2
Chanpreet Singh <ch835893@dal.ca>
2022-07-12 18:21:17 -0300

    app

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ from dynamoUtilities import DynamoUtilities
 
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"*":{"origins":"*"}})
+# app.config['CORS_HEADERS'] = 'Content-Type'
 s3_obj = S3Utilities(access_key_id=constants.aws_access_key_id, secret_access_key=constants.aws_secret_access_key, session_key=constants.aws_session_token)
 dynamo_obj = DynamoUtilities(access_key_id=constants.aws_access_key_id, secret_access_key=constants.aws_secret_access_key, session_key=constants.aws_session_token)
 redis_obj = redis.Redis(host= 'localhost', port= 6379)
@@ -34,12 +34,14 @@ with open(constants.model_output_folder + constants.model_file_name, "rb") as fp
 embedder = SentenceTransformer('all-MiniLM-L6-v2')
 
 @app.route("/train_updated_data", methods=['POST'])
+@cross_origin()
 def initiate_training():
     st = time.time()
     TrainModel().main()
     return jsonify({"message": "Model successfully trained in {0} seconds. Restart the service in the server to load the new model!".format(int(time.time() - st))}), 200
 
 @app.route("/get_relevant_docs", methods=["GET"])
+@cross_origin()
 def get_relevant_data():
     jsonPayload = request.get_json()
     text = jsonPayload.get("text", None)
@@ -50,39 +52,42 @@ def get_relevant_data():
 
         return jsonify(result), 200
 
-@app.route("/get_response_1", methods=["GET", "OPTIONS"])
+@app.route("/get_response_1", methods=["GET"])
 @cross_origin()
 def get_response_1():
     jsonPayload = request.get_json()
     text = jsonPayload.get("text", None)
     session_id = jsonPayload.get("session_id", None)
-    if request.method == "OPTIONS":
-        return _build_cors_preflight_response()
-    if not text:
-        return jsonify({"message": "Send 'text' parameter in body"}), 400
-    if not session_id:
-        return jsonify({"message": "Send 'session_id' parameter in body"}), 400
-    else:
-        resp = get_response_from_lex(text)
+    # if request.method == "OPTIONS":
+    #     return _build_cors_preflight_response()
+    # if not text:
+    #     return jsonify({"message": "Send 'text' parameter in body"}), 400
+    # if not session_id:
+    #     return jsonify({"message": "Send 'session_id' parameter in body"}), 400
+    # else:
+    resp = get_response_from_lex(text)
 
-        if resp is None or resp == "Abra Ka Dabra":
-            if resp is None:
-                print("No response from Lex")
-            elif resp == "Abra Ka Dabra":
-                print("Got default response from Lex")
-            result = TestModel().test(metadata, embeddings, text, embedder, constants.top_document_count)
-            tags = list(set([each["source_tag"] for each in result]))
-            if len(tags)>1:
-                redis_obj.set(session_id, json.dumps(result), constants.redis_ttl)
-                m = random.choice(["Found some articles, on which genre?", "Ammmm..! Which one?", "I am confused :/ On which genre?"])
-                return Response(headers={'Access-Control-Allow-Origin': '*'}, status=200, response=json.dumps({"message": m, "hidden_message": True, "tags": tags, "data_to_display": None}))
-            else:
-                m = random.choice(["Found something..", "Voila..!", "Bingo.!.", "I hope you like these.."])
-
-                return Response(headers={'Access-Control-Allow-Origin': '*'}, status=200, response=json.dumps(
-                    {"message": m, "hidden_message": None, "tags": [], "data_to_display": result}))
+    if resp is None or resp == "Abra Ka Dabra":
+        if resp is None:
+            print("No response from Lex")
+        elif resp == "Abra Ka Dabra":
+            print("Got default response from Lex")
+        result = TestModel().test(metadata, embeddings, text, embedder, constants.top_document_count)
+        tags = list(set([each["source_tag"] for each in result]))
+        if len(tags)>1:
+            redis_obj.set(session_id, json.dumps(result), constants.redis_ttl)
+            m = random.choice(["Found some articles, on which genre?", "Ammmm..! Which one?", "I am confused :/ On which genre?"])
+            return jsonify({"message": m, "hidden_message": True, "tags": tags, "data_to_display": None}), 200
+            # return Response(headers={'Access-Control-Allow-Origin': '*'}, status=200, response=json.dumps({"message": m, "hidden_message": True, "tags": tags, "data_to_display": None}))
         else:
-            return Response(headers={'Access-Control-Allow-Origin': '*'}, status=200, response=json.dumps({"message": resp, "hidden_message": None, "tags": [], "data_to_display": None}))
+            m = random.choice(["Found something..", "Voila..!", "Bingo.!.", "I hope you like these.."])
+            return jsonify({"message": m, "hidden_message": None, "tags": [], "data_to_display": result}), 200
+
+            # return Response(headers={'Access-Control-Allow-Origin': '*'}, status=200, response=json.dumps(
+            #     {"message": m, "hidden_message": None, "tags": [], "data_to_display": result}))
+    else:
+        return jsonify({"message": resp, "hidden_message": None, "tags": [], "data_to_display": None}), 200
+        # return Response(headers={'Access-Control-Allow-Origin': '*'}, status=200, response=json.dumps({"message": resp, "hidden_message": None, "tags": [], "data_to_display": None}))
 
 @app.route("/get_response_2", methods=["GET", "OPTIONS"])
 @cross_origin()
@@ -90,24 +95,25 @@ def get_response_2():
     jsonPayload = request.get_json()
     text = jsonPayload.get("text", None)
     session_id = jsonPayload.get("session_id", None)
-    if request.method == "OPTIONS":
-        return _build_cors_preflight_response()
-    if not text:
-        return jsonify({"message": "Send 'text' parameter in body. This will be the tag that you will fetch from the user."}), 400
-    if not session_id:
-        return jsonify({"message": "Send 'session_id' parameter in body. This session_id will be the same that you have already sent in get_response_1."}), 400
-    else:
-        redis_data = redis_obj.get(session_id)
-        if redis_data:
-            redis_data = json.loads(redis_data)
-            data_to_return = [each for each in redis_data if each["source_tag"]==text]
-            if data_to_return:
-                m = random.choice(["Found something..", "Voila..!", "Bingo.!.", "I hope you like these.."])
-            else:
-                data_to_return = redis_data
-                m = random.choice(["Ahhh..there you go!", "I think you meant all..", "Okay okay got it, just have a look at these.."])
-            redis_obj.delete(session_id)
-            return Response(headers={'Access-Control-Allow-Origin': '*'}, status=200, response=json.dumps({"message": m, "hidden_message": None, "tags": [], "data_to_display": data_to_return}))
+    # if request.method == "OPTIONS":
+    #     return _build_cors_preflight_response()
+    # if not text:
+    #     return jsonify({"message": "Send 'text' parameter in body. This will be the tag that you will fetch from the user."}), 400
+    # if not session_id:
+    #     return jsonify({"message": "Send 'session_id' parameter in body. This session_id will be the same that you have already sent in get_response_1."}), 400
+    # else:
+    redis_data = redis_obj.get(session_id)
+    if redis_data:
+        redis_data = json.loads(redis_data)
+        data_to_return = [each for each in redis_data if each["source_tag"]==text]
+        if data_to_return:
+            m = random.choice(["Found something..", "Voila..!", "Bingo.!.", "I hope you like these.."])
+        else:
+            data_to_return = redis_data
+            m = random.choice(["Ahhh..there you go!", "I think you meant all..", "Okay okay got it, just have a look at these.."])
+        redis_obj.delete(session_id)
+        return jsonify({"message": m, "hidden_message": None, "tags": [], "data_to_display": data_to_return}), 200
+        # return Response(headers={'Access-Control-Allow-Origin': '*'}, status=200, response=json.dumps({"message": m, "hidden_message": None, "tags": [], "data_to_display": data_to_return}))
 
 def _build_cors_preflight_response():
     response = make_response()
